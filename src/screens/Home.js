1553845af1c0c401dd532d6db555f77fa927fdb5
D4ve-Code <78406260+D4ve-Code@users.noreply.github.com>
2023-01-05 20:23:54 +0100

    creation  Header & add assets_img

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header.js'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>ici termine ma Home</h1>
+    </div>
+  )
+}
+
+export default Home
