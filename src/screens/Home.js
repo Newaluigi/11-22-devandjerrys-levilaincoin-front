43f78641b6606d99b570c3531ae19d788922e47e
Marion TASSY <marion2macedo@gmail.com>
2023-01-06 10:19:51 +0100

    add component service and pages services without links

---
+++ src/screens/Home.js
@@ -0,0 +1,32 @@
+import Service from '../components/Service'
+import './Home.css'
+// import { Routes, Route } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      <section id='home-cardService'>
+        <Service
+          img='/conquer.png'
+          alt='conquer'
+          details='Conquérir le monde'
+        />
+        <Service img='/escort.png' alt='escort' details='Escort vilain' />
+        <Service
+          img='/birthday.png'
+          alt='birthday'
+          details="Fête d'anniversaire"
+        />
+        <Service img='/nanny.png' alt='nanny' details='Bad nounou' />
+        <Service img='/evjf.png' alt='stag' details='EVG /EVJF' />
+        <Service
+          img='/destroy.png'
+          alt='destroy'
+          details='Dégradation en tout genre'
+        />
+      </section>
+    </div>
+  )
+}
+
+export default Home
