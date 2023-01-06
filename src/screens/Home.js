b74f7e928f0107fdf94b41985bfea70bbe36ed32
Noiramd31 <marion2macedo@gmail.com>
2023-01-06 12:13:44 +0100

    update saas and folders

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,5 @@
 import Service from '../components/Service'
 import Header from '../components/Header.js'
-import './Home.css'
 // import { Routes, Route } from 'react-router-dom'
 
 const Home = () => {
@@ -9,20 +8,32 @@ const Home = () => {
       <Header />
       <section id='home-cardService'>
         <Service
-          img='/conquer.png'
+          img={require('../assets/img/conquer.png')}
           alt='conquer'
           details='Conquérir le monde'
         />
-        <Service img='/escort.png' alt='escort' details='Escort vilain' />
         <Service
-          img='/birthday.png'
+          img={require('../assets/img/escort.png')}
+          alt='escort'
+          details='Escort vilain'
+        />
+        <Service
+          img={require('../assets/img/birthday.png')}
           alt='birthday'
           details="Fête d'anniversaire"
         />
-        <Service img='/nanny.png' alt='nanny' details='Bad nounou' />
-        <Service img='/evjf.png' alt='stag' details='EVG /EVJF' />
         <Service
-          img='/destroy.png'
+          img={require('../assets/img/nanny.png')}
+          alt='nanny'
+          details='Bad nounou'
+        />
+        <Service
+          img={require('../assets/img/evjf.png')}
+          alt='stag'
+          details='EVG /EVJF'
+        />
+        <Service
+          img={require('../assets/img/destroy.png')}
           alt='destroy'
           details='Dégradation en tout genre'
         />
