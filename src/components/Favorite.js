e60115e4b10f2dfa447f9be478b0c809e2e2a330
Newaluigi <alexandre.levillain@hotmail.com>
2023-01-10 17:41:07 +0100

    new element Fav

---
+++ src/components/Favorite.js
@@ -0,0 +1,12 @@
+const Favorite = () => {
+  return (
+    <div className='favClick'>
+      <span className='fa fa-star-o'></span>
+      <div className='ring'></div>
+      <div className='ring2'></div>
+      <p className='info'>Ajouté à vos favoris!</p>
+    </div>
+  )
+}
+
+export default Favorite
