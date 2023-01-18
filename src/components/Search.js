25f00e1a3fff4c16db3c91f094097c9c8785f829
Moana.A <moana.amis@gmail.com>
2023-01-18 18:25:38 +0100

    modification: SearchlList/footer/Search/Header .js modification: SearchlList/footer/Search/Header . scss media queries on search bar footer & useState display/hide on glasspicture & display of search bar in footer Header scss modified as well because of conflict between mobile/desktop versions Refactoring of Search/Searchlist as components usable by both header & footer

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,56 @@
-const Search = (props) => {
-  const { search, setSearch } = props;
+// import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import SearchList from './SearchList';
+
+// const Search = (props) => {
+//   const { search, setSearch } = props;
+  const Search = () => {
+
+  // XXXXXXXXXXXXXXX initial state, full list XXXXXXXXXXXXXXXXX
+  const [vilains, setVilains] = useState([])
+  // UseEffect
+  useEffect(() => {
+    axios.get('http://localhost:4242/').then(response => {
+      console.log(response.data);
+      setVilains(response.data)
+      console.log(vilains);
+    })
+  }, [])
+  // XXXXXXXXXXXXXXXX state of the vilain List display  XXXXXXXXXXXXXXXXXXXXXXXXXX
+  const [search, setSearch] = useState("");
+  const vilainsDisplayed = vilains;
+
+  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  const [isGlassDisplayed, setIsGlassDisplayed] = useState("");
+  const [isInputDisplayed, setisInputDisplayed] = useState('');
+
+  const handleClickGlass = (e) =>{
+    console.log('cliclikkk')
+    setIsGlassDisplayed("none");
+    setisInputDisplayed("initial");
+    console.log(typeof isGlassDisplayed);
+    console.log(isInputDisplayed);
+  }
+
   return (
     <div className='searchModule'>
       <img
         className='glassPicture'
         src={require('../assets/img/glass.png')}
         alt='loupe'
+        onClick={handleClickGlass}
+        style= {{display: isGlassDisplayed}}
       />
             <input className="inputSearch"
                 value={search}
                 type="text"
                 placeholder="..."
                 onChange={(e) => setSearch(e.target.value)}
+                style= {{display: isInputDisplayed}}
             />
+            <SearchList search={search} vilainsDisplayed={vilainsDisplayed} />
     </div>
   )
 }
