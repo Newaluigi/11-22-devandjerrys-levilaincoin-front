efed978e9e813fc0900b91e20d0f26738968b78f
Moana.A <moana.amis@gmail.com>
2023-01-19 22:43:19 +0100

    add: component CustomHookClickOutside for detecting clicks outside search list element and close it. modification: ElementListSearch ref and custom hook

--- src/components/ElementListSearchbar.js
+++ src/components/ElementListSearchbar.js
@@ -1,19 +1,31 @@
 import { Link } from 'react-router-dom'
-
+import { useRef } from 'react';
+import CustomHookClickOutside from './CustomHookClickOutside';
 
 const ElementListSearchbar = (props) => {
+
     const { vilainsDisplayed, setSearch, setIsGlassDisplayed, setisInputDisplayed } = props;
     const idVilain = vilainsDisplayed.id;
 
-    // XXXXXXXX Click event on selected character? go to profile page XXXXXXXXXXXXXXXXXXX
-    // XXXXXXXXX Mobile version close searchbar open glass picture XXXXXXXXXXXXXXXXXXXXXX
+    const searchResult = useRef();
+
+    // XXXXXXXXXXX Click  on selected character==> go to profile page XXXXXXXXXXXXXXXXXXX
+    // XXXXXXXXX  + Mobile version close searchbar open glass picture XXXXXXXXXXXXXXXXXXX
     const closeSearchList = (e) => {
         e.preventDefault()
         setisInputDisplayed('')
         setIsGlassDisplayed("")
         setSearch("")
-        console.log('lllllll');
     }
+// XXXXXXXXXXXX Custom HOOk call closes search onClick anywhere out of listXXXXXXXXXXXXX
+
+    CustomHookClickOutside(searchResult,() =>{
+        
+        setisInputDisplayed('')
+        setIsGlassDisplayed("")
+        setSearch("")
+    });
+
     return (
         <>
             
@@ -21,6 +33,7 @@ const ElementListSearchbar = (props) => {
                 className='searchResult'
                 key={vilainsDisplayed.id}
                 onClick={closeSearchList}
+                ref={searchResult}
                         >
                 <Link to={`/profile/id/${idVilain}`}>
                     <div className='selection' >
