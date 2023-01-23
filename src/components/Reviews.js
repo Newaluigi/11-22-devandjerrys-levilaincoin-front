a769b6890be7d45d665d352041894ef9828150ea
D4ve-Code <78406260+D4ve-Code@users.noreply.github.com>
2023-01-23 17:40:25 +0100

    addReviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -3,18 +3,114 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { BiLogInCircle } from 'react-icons/bi'
 
-const reviews = () => {
+import RandomReview from './RandomReview'
+
+const Reviews = () => {
+  const [vilainInfo1, setVilainInfo1] = useState([])
+  const [vilainInfo2, setVilainInfo2] = useState([])
+  function getRandomInt() {
+    const min = Math.ceil(1)
+    const max = Math.floor(62)
+    return Math.floor(Math.random() * (max - min) + 1)
+  }
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4242/selection/id/${getRandomInt()}`)
+      .then(response => setVilainInfo1(response.data))
+    axios
+      .get(`http://localhost:4242/selection/id/${getRandomInt()}`)
+      .then(response => setVilainInfo2(response.data))
+  }, [])
+
   return (
     <div className='reviewsBlock'>
-      <h2>X Commentaires</h2>
-      <div>
-        <div className='firstReview'>1er review</div>
-        <div className='secondReview'>2e review</div>
+      <h1>2 Commentaires</h1>
+      {/*-----------1ere review ------------ */}
+      <div className='reviewsAndArrow'>
+        <div className='randomReviews'>
+          <div className='randomReview1'>
+            <div className='randomReviewProfile1'>
+              <BiLogInCircle className='randomReviewImage1' />
+              <div className='randomReviewInformations1'>
+                <h2>firstName</h2>
+                <h3>Date</h3>
+              </div>
+            </div>
+            <div className='reviewFromApi'>
+              {vilainInfo1
+                ? vilainInfo1.map(vilainInfo1 => {
+                    return (
+                      <RandomReview
+                        key={vilainInfo1.id}
+                        comments={vilainInfo1.comments}
+                      />
+                    )
+                  })
+                : null}
+            </div>
+          </div>
+          {/*-----------2e review ------------ */}
+          <div className='randomReview2'>
+            <div className='randomReviewProfile2'>
+              <BiLogInCircle className='randomReviewImage2' />
+              <div className='randomReviewInformations2'>
+                <h2>firstName</h2>
+                <h3>Date</h3>
+              </div>
+            </div>
+            <div className='reviewFromApi'>
+              {vilainInfo2
+                ? vilainInfo2.map(vilainInfo2 => {
+                    return (
+                      <RandomReview
+                        key={vilainInfo2.id}
+                        comments={vilainInfo2.comments}
+                      />
+                    )
+                  })
+                : null}
+            </div>
+          </div>
+        </div>
+        <HiArrowNarrowRight className='logoArrow' />
       </div>
-      <HiArrowNarrowRight className='logoArrow' />
     </div>
   )
 }
 
-export default reviews
+export default Reviews
+
+// {/* <>
+// <div className='reviewsBlock'>
+//   <h2>X Commentaires</h2>
+//   <div className='reviewsFromApi'>
+//     {/*---------------------  1ere review dans laquelle je dois appeler l'API -----------*/}
+//     <div className='firstReview'>
+//       <img className='profilePicture'>
+//         <BiLogInCircle />
+//       </img>
+//       <div className='profileInformations'>
+//         <h3>Fake name</h3>
+//         <p>Date de la review</p>
+//       </div>
+//       <p className='contentFromApi'>Ici le contenu d'appel API comment</p>
+//     </div>
+//     {/*---------------------  2e review dans laquelle je dois appeler l'API -----------*/}
+//     <div className='secondReview'>
+//       2e review
+//       <img className='profilePicture'>
+//         <BiLogInCircle />
+//       </img>
+//       <div className='profileInformations'>
+//         <h3>Fake name</h3>
+//         <p>Date de la review</p>
+//       </div>
+//       <p className='contentFromApi'>Ici le contenu d'appel API comment</p>
+//     </div>
+//     {/*------------------- Icone flèche ---------------- */}
+//     <HiArrowNarrowRight className='logoArrow' />
+//   </div>
+// </div>
+// </> */}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,52 +13,55 @@ const Home = () => {
         alt='banner du site'
       ></img>
       {/* ----------------------- Les catégories -------------------- */}
-      <section id='home-cardService'>
-        <Link to='/conquer' className='linkCardService'>
-          <Service
-            img={require('../assets/img/conquer.png')}
-            alt='conquer'
-            details='Conquérir le monde'
-          />
-        </Link>
-        <Link to='/escort' className='linkCardService'>
-          <Service
-            img={require('../assets/img/escort.png')}
-            alt='escort'
-            details='Escort vilain'
-          />
-        </Link>
-        <Link to='/birthday' className='linkCardService'>
-          <Service
-            img={require('../assets/img/birthday.png')}
-            alt='birthday'
-            details="Fête d'anniversaire"
-          />
-        </Link>
-        <Link to='/nanny' className='linkCardService'>
-          <Service
-            img={require('../assets/img/nanny.png')}
-            alt='nanny'
-            details='Bad nounou'
-          />
-        </Link>
-        <Link to='/stag' className='linkCardService'>
-          <Service
-            img={require('../assets/img/evjf.png')}
-            alt='stag'
-            details='EVG et EVJF'
-          />
-        </Link>
-        <Link to='/destroy' className='linkCardService'>
-          <Service
-            img={require('../assets/img/destroy.png')}
-            alt='destroy'
-            details='Dégradation'
-          />
-        </Link>
-      </section>
-      {/* ----------------------- les Reviews -------------------- */}
-      <Reviews />
+      <div className='rightContent'>
+        {/* <section id='home-cardService'>
+          <Link to='/conquer' className='linkCardService'>
+            <Service
+              img={require('../assets/img/conquer.png')}
+              alt='conquer'
+              details='Conquérir le monde'
+            />
+          </Link>
+          <Link to='/escort' className='linkCardService'>
+            <Service
+              img={require('../assets/img/escort.png')}
+              alt='escort'
+              details='Escort vilain'
+            />
+          </Link>
+          <Link to='/birthday' className='linkCardService'>
+            <Service
+              img={require('../assets/img/birthday.png')}
+              alt='birthday'
+              details="Fête d'anniversaire"
+            />
+          </Link>
+          <Link to='/nanny' className='linkCardService'>
+            <Service
+              img={require('../assets/img/nanny.png')}
+              alt='nanny'
+              details='Bad nounou'
+            />
+          </Link>
+          <Link to='/stag' className='linkCardService'>
+            <Service
+              img={require('../assets/img/evjf.png')}
+              alt='stag'
+              details='EVG et EVJF'
+            />
+          </Link>
+          <Link to='/destroy' className='linkCardService'>
+            <Service
+              img={require('../assets/img/destroy.png')}
+              alt='destroy'
+              details='Dégradation'
+            />
+          </Link>
+        </section> */}
+
+        {/* ----------------------- les Reviews -------------------- */}
+        <Reviews className='sectionReviews' />
+      </div>
     </div>
   )
 }
