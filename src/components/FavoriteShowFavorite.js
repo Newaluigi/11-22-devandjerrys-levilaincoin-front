7c8d893b1c07adeeba8fac2e741d3566d5da0e03
Moana.A <moana.amis@gmail.com>
2023-01-29 16:28:45 +0100

    add: tabHistos state in FavoriteShowFavorite, fav status now dynamic within the state. next step: overwrite tabHistos with new fav status before reload on screen.

--- src/components/FavoriteShowFavorite.js
+++ src/components/FavoriteShowFavorite.js
@@ -1,15 +1,37 @@
 import { logDOM } from '@testing-library/react';
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function FavoriteShowFavorite(props) {
 
+  const [tabHistos, setTabHistos] = useState([])
+  const [displayed, setDisplayed] = useState(props.vilainInfo1)
+  useEffect(() => {
+    const tabHistos = JSON.parse(localStorage.getItem('histo'));
+    setTabHistos(tabHistos);
+    // console.log(tabHistos);
+    // localStorage.setItem('favorite', [1,3 ,5] ); 
+  }, [])
+
 const changeFavStatus = () =>{
-  // console.log("change")
+  // this line changes the state that trigger the useEffect reload on UserProfilePage
   props.changeFavoriteStatus(!props.changeFavorite);
-  console.log(props.vilainInfo1);
-  props.vilainInfo1[2] = (!props.changeFavorite);
-  console.log(props.vilainInfo1[2]);
-  console.log(props.userHisto[0].id);
+  // Here's the change of the favorite status in the imported database tabHistos
+  // at first change the fav status in vilainInfo1 which is the component character
+  displayed.favorite=(!props.changeFavorite);
+  // console.log(displayed);
+  // Now change THIS character fav status in the main database using .map
+  tabHistos.map((tabHisto)=>{tabHisto.id===displayed.id? tabHisto.favorite=displayed.favorite:null})
+
+// tabHistos.map((tabHisto)=>{console.log(tabHisto.id);
+//   console.log(displayed.id); })
+
+
+  // tabHistos[0].favorite = (!props.changeFavorite);
+  // console.log(props.vilainInfo1);
+  // console.log(props.vilainInfo1[2]);
+  // console.log(props.userHisto[0].id);
+  // console.log(tabHistos[2]);
+  console.log(tabHistos);
 }
 
 
